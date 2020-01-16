3c142845c8e62217499971b95c863ec265a85a89
Cyril <pro.cyril.reymond@gmail.com>
2020-01-16 17:28:46 +0100

    Adding mutation on genotype

--- src/configuration.py
+++ src/configuration.py
@@ -1,3 +1,8 @@
 PARAMETERS = {
-
+    'mutation': 0.5,
+    'crossover': 0.5,
+    'selection': 0.05,
+    'length_min': 12,
+    'length_max': 18,
+    'population_initial': 10
 }
--- src/genotype.py
+++ src/genotype.py
@@ -1,6 +1,7 @@
 # chaîne de int (changer char en int avec orb())
 # mutation
 import random
+from configuration import *
 
 
 class Genotype:
@@ -13,21 +14,44 @@ class Genotype:
 
     def initialize_phenotype(self):
         for i in range(self.length_max):
-            self.phenotype.append(self.generate_gene())
+            self.phenotype.append(self.generate_trait())
 
     def initialize_genotype(self):
         for i in range(len(self.phenotype)):
             self.genotype.append(ord(self.phenotype[i]))
 
-    def generate_gene(self):
+    def generate_trait(self):
         nb = random.randint(48, 57)
         letter = random.randint(65, 90)
         value = random.choice([nb, letter])
         return chr(value)
 
+    def generate_gene(self):
+        nb = random.randint(48, 57)
+        letter = random.randint(65, 90)
+        value = random.choice([nb, letter])
+        return value
+
     def replace_char(self):
         for i in range(len(self.genotype)):
             self.genotype[i] = ord(self.genotype[i])
+
+    def update_phenotype(self):
+        for i in range(len(self.phenotype)):
+            self.phenotype[i] = chr(self.genotype[i])
+
+    def mutation(self):
+        threshold_mutation = PARAMETERS['mutation']
+        for i in range(len(self.genotype)):
+            r = random.random()
+            if r <= threshold_mutation:
+                # faire mut
+                self.genotype[i] = self.generate_gene()
+        self.update_phenotype()
+
+
+    def __len__(self):
+        return len(self.phenotype)
 
 
 if __name__ == "__main__":
