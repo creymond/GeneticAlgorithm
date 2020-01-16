7ea6123ea139586c132ad6d42285a72e93f03af0
claire <clairenatacha.pillet@free.fr>
2020-01-16 16:13:58 +0100

    iindividu ok, premiere pop

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,24 +1,20 @@
 from blackbox37 import check
 
-similarity = check(4, 'AAAAAAAAAAAAAAAA')
-print(similarity)
-
+from genotype import Genotype
 
 class Individu:
     def __init__(self, length_max, length_min):
         self.length_max = length_max
         self.length_min = length_min
+        self.fitness = -1
+        self.genotype = Genotype()
+        self.fitness = check(4, "AAAAAAAAAAAAA")
 
 
 
-    def compare_fitness(self):
-        pass
 
-    def mutation(self):
-        pass
 
-    def crossover(self):
-        pass
+
 
 
 
--- src/population.py
+++ src/population.py
@@ -1,10 +1,52 @@
+from operator import itemgetter
+
+from algorithm import Individu
+import numpy as np
 
 class Population:
-    def __init__(self):
+    def __init__(self, nb, percent):
         self.individus = []
+        self.individusBest = []
+        self.percent = percent
+        self.nb = nb
+        for lettre in range(nb):
+            ind = Individu(18, 12)
+            self.individus.append(ind)
+        self.evaluation()
 
     def cross_over(self):
         pass
 
     def generate_child(self, parent1, parent2):
-        # générer
+
+        pass
+
+    def generate_newPop(self):
+        self.individus.clear()
+        self.generate_child(self, parent1, parent2)
+
+        # clean elite
+        self.individusBest.clear()
+        self.evaluation()
+        pass
+    def evaluation(self):
+        lst=[]
+        for ind in self.individus:
+            tulple= (ind,ind.fitness)
+            lst.append(tulple)
+
+        i=0
+        while i<=(len(self.individus)/self.percent):
+            tulpleM=max(lst, key=itemgetter(1))
+            self.individusBest.append(tulpleM[0])
+            print(tulpleM[0].fitness)
+            lst.remove(tulpleM)
+            i+=1
+
+
+
+if __name__ == "__main__":
+    pop = Population(10,4)
+    pop.generate_newPop()
+    #pop.generatenew pop
+    #
